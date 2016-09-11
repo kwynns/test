090b7411adc237ccc0157de3d21341054e26e8a4
tgragg <tgragg@changehealthcare.com>
2016-09-11 18:19:59 -0500

    delete movie button works

--- src/js/reducers/movieList.js
+++ src/js/reducers/movieList.js
@@ -18,6 +18,15 @@ export default function (state = initialState, action) {
     case types.REMOVE_MOVIE_BY_SHOW_ID:{
       console.log('show id to remove', action.id);
       let newState = R.clone(state);
+      let movies = newState.movies;
+
+      for(var i = 0; i < movies.length; i++) {
+          if(movies[i].show_id == action.id) {
+              movies.splice(i, 1);
+              break;
+          }
+      }
+      newState.movies = movies
       // remove the movie with the show_id equal to action.id from newState
       // if succesfull, the movie will be removed when you click the trash icon next to it
       return newState;
