414614086202f9a01cb6d91ec3d508638f3f90ad
Kelsey Wynns <kelseywynns@gmail.com>
2019-02-06 19:12:11 -0600

    moe

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,9 @@ import { Provider } from 'react-redux';
 import configureStore  from './store/configureStore';
 import { Router, browserHistory } from 'react-router';
 
+
+
+
 import routes from './routes';
 console.log('making a change')
 const store = configureStore();
